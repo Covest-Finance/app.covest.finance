5e45d9437bfa7de476242acd6cc3b4320dd9d315
BeamNawapat <nawapatb.2b@hotmail.com>
2022-06-07 23:05:09 +0000

    fixed style - Automatic publish

--- src/views/Pool/index.js
+++ src/views/Pool/index.js
@@ -2,12 +2,12 @@ import TopSection from "./PoolView/TopSection";
 import MainSection from "./PoolView/MainSection";
 import useGetPool from "../../hooks/useGetPool";
 const Index = () => {
-    return (
-        <div style={{ minHeight: "85vh", width: "100%" }}>
-            <TopSection></TopSection>
-            <MainSection></MainSection>
-        </div>
-    );
+  return (
+    <div style={{ minHeight: "87vh", width: "100%" }}>
+      <TopSection></TopSection>
+      <MainSection></MainSection>
+    </div>
+  );
 };
 
 export default Index;
